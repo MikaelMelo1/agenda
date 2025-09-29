8b7da0163eb5fb414eddd5816597b7da0b1565a6
Mikael Melo <mikaelsilva1940@hotmail.com>
2025-09-29 16:21:05 -0300

    fix: ajustes de URL BASE

--- templates/header.php
+++ templates/header.php
@@ -2,7 +2,13 @@
 
   include_once("./config/url.php");
   include_once("./config/connection.php");
-
+  //mensagem de sessao
+  $printMsg = '';
+  //limpa mensagem
+  if (isset($_SESSION['msg']) && $_SESSION['msg'] != '') {
+    $printMsg = $_SESSION['msg'];
+    $_SESSION['msg'] = '';
+  }
 ?>
 
 <!DOCTYPE html>
@@ -12,8 +18,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.3.8/css/bootstrap.min.css" integrity="sha512-2bBQCjcnw658Lho4nlXJcc6WkV/UxpE/sAokbXPxQNGqmNdQrWqtw26Ns9kFF/yG792pKR1Sx8/Y1Lf1XN4GKA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-  <link rel="stylesheet" href="<?= $BASE_URL ?>css/styles.css">
-  
+  <link rel="stylesheet" href="<?= $BASE_URL ?>/css/styles.css">  
   <title>Agenda de Contatos</title>
 </head>
 <body>
@@ -24,8 +29,8 @@
       <img src="<?= $BASE_URL ?>img/logo.svg" alt="Agenda">
     </a>
     <div class="navbar-nav">
-      <a href="<?= $BASE_URL ?>index.php" class="nav-link active" id="home- active">Agenda</a>
-      <a href="<?= $BASE_URL ?>create.php" class="nav-link active" id="home-link">Adicionar Contato</a>
+      <a href="<?= $BASE_URL ?>index.php" class="nav-link active" id="home-link">Agenda</a>
+      <a href="<?= $BASE_URL ?>create.php" class="nav-link active" id="">Adicionar Contato</a>
     </div>
   </nav>
 </header>
